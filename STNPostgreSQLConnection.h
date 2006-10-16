b2eda9705d9f6d482cb7dee39df16296b33fcbfe
Simon Stiefel <simon@stiefels.net>
2006-10-16 18:58:31 +0000

    initial import
    git-svn-id: http://stiefels.net/svn/STNPostgreSQL/trunk@1 5e87bd37-f21f-0410-93ab-ca7b9b871d09

---
+++ STNPostgreSQLConnection.h
@@ -0,0 +1,16 @@
+//
+//  STNPostgreSQLConnection.h
+//  STNPostgreSQL
+//
+//  Created by Simon Stiefel on 16.10.06.
+//  Copyright 2006 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface STNPostgreSQLConnection : NSObject {
+
+}
+
+@end
---
+++ STNPostgreSQLConnection.m
@@ -0,0 +1,14 @@
+//
+//  STNPostgreSQLConnection.m
+//  STNPostgreSQL
+//
+//  Created by Simon Stiefel on 16.10.06.
+//  Copyright 2006 __MyCompanyName__. All rights reserved.
+//
+
+#import "STNPostgreSQLConnection.h"
+
+
+@implementation STNPostgreSQLConnection
+
+@end
